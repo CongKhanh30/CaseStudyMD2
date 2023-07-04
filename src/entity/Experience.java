e7c949d72e5aa739e2a88073952aa15fb14f64dd
CongKhanh30 <khanhnc45@wru.vn>
2023-07-04 21:16:29 +0700

    Update

--- src/entity/Experience.java
+++ src/entity/Experience.java
@@ -1,4 +1,54 @@
 package entity;
 
-public class Experience {
+import java.time.LocalDate;
+
+public class Experience extends Employee {
+    private int expInYear;
+    private String skill;
+
+    public Experience(){}
+
+    public Experience(int expInYear, String skill) {
+        this.expInYear = expInYear;
+        this.skill = skill;
+    }
+
+    public Experience(int id, String fullName, LocalDate birthday, String phone, String email, int employeeType, int expInYear, String skill) {
+        super(id, fullName, birthday, phone, email, employeeType);
+        this.expInYear = expInYear;
+        this.skill = skill;
+    }
+
+    @Override
+    public void showInfo() {
+
+    }
+
+    public int getExpInYear() {
+        return expInYear;
+    }
+
+    public void setExpInYear(int expInYear) {
+        this.expInYear = expInYear;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    @Override
+    public String toString() {
+        return "Nhân viên lâu năm: " +
+                ", Id: " + id +
+                ", Họ và tên: " + fullName + '\'' +
+                ", Ngày sinh: " + birthday +
+                ", Số điện thoại: " + phone + '\'' +
+                ", Email: " + email + '\'' +
+                ", Năm kinh nghiệm: " + expInYear +
+                ", Kỹ năng: " + skill + '\'';
+    }
 }
--- src/entity/Intern.java
+++ src/entity/Intern.java
@@ -1,4 +1,66 @@
 package entity;
 
-public class Intern {
+import java.time.LocalDate;
+
+public class Intern extends Employee {
+
+    private String majors;
+    private int schoolYear;
+    private String universityName;
+     public Intern(){}
+
+    public Intern(String majors, int schoolYear, String universityName) {
+        this.majors = majors;
+        this.schoolYear = schoolYear;
+        this.universityName = universityName;
+    }
+
+    public Intern(int id, String fullName, LocalDate birthday, String phone, String email, int employeeType, String majors, int schoolYear, String universityName) {
+        super(id, fullName, birthday, phone, email, employeeType);
+        this.majors = majors;
+        this.schoolYear = schoolYear;
+        this.universityName = universityName;
+    }
+
+    @Override
+    public void showInfo() {
+
+    }
+
+    public String getMajors() {
+        return majors;
+    }
+
+    public void setMajors(String majors) {
+        this.majors = majors;
+    }
+
+    public int getSchoolYear() {
+        return schoolYear;
+    }
+
+    public void setSchoolYear(int schoolYear) {
+        this.schoolYear = schoolYear;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
+
+    @Override
+    public String toString() {
+        return "Intern: " +
+                ", ID: " + id +
+                ", Họ và tên: " + fullName + '\'' +
+                ", Ngày sinh: " + birthday +
+                ", Số điện thoại: " + phone + '\'' +
+                ", Email: " + email + '\'' +
+                ", Chuyên ngành: " + majors + '\'' +
+                ", Năm học: " + schoolYear +
+                ", Đại học: " + universityName + '\'';
+    }
 }
